54fe35228a2149c779c43eda4f5ef53391fca0f1
Hamidrahim07 <145069844+Hamidrahim07@users.noreply.github.com>
2025-04-14 19:43:03 +0500

    Add files via upload

---
+++ 02_operators.py
@@ -0,0 +1,27 @@
+print(2+3)#addition
+print(3*5)#mutiple
+print(4/2) #you get a float number (whenever you have to get value in points)
+print(4-1)#subtractions
+print(13%2) # remainder after division
+print(6//2) #you wil get INT(whenever you have to get values in whole number)
+print(2**3) #exponent
+print(2**4/5+(2*5)+4/5)
+print(13%2)
+print(3**2/2*3/3+6-4/(2*6))
+#Assignment
+#what is type casting? and how we use that in python 
+#boolen operator/ binary data search it on google
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
